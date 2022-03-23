ddf477b023ce603ea9627062e7d862b79fd41f6e
lmszymborski <lmszymborski@wisc.edu>
2022-03-23 15:02:53 -0500

    finished

--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,9 @@ import React from "react";
 import "./App.css";
 import Course from "./Course";
 
+/*
+
+*/
 class Home extends React.Component {
 render() {
     return <div id="home">
@@ -12,7 +15,6 @@ render() {
         <p>Browse through courses and limit your search results by using filters such as by keyword, subject, or credit number. Add courses to your cart by clicking the "heart" icon.</p>
         <h2 class="title">Completed Courses</h2>
         <p>View courses that you have already taken. View their description by clicking "View Description". Rate the class 1 out of 5 by clicking on the star rating.</p>
-
         </div>
     </div>;
   }
